6e606e21e61e2bf8293ab7a3ab999b7f9a8f7335
Wayne K Seymour III <wayneseymour@gmail.com>
2014-07-28 20:53:14 -0400

    Added more existy() specs from Micheal Fogus && version bump.

--- index.spec.js
+++ index.spec.js
@@ -15,6 +15,27 @@ describe('Existy', function() {
     expect(existy('undefined')).toEqual(true);
     done();
   });
+
+  it("should respond with false when applied to an undefined object property", function(done) {
+    expect(existy({}.notHere)).toEqual(false);
+    done();
+  });
+
+  it("should respond with false when applied to the result of an IIFE", function(done) {
+    expect(existy((function() {})())).toEqual(false);
+    done();
+  });
+
+  it("should respond with true when applied to the number 0", function(done) {
+    expect(existy(0)).toEqual(true);
+    done();
+  });
+
+  it("should respond with true when applied the false keyword", function(done) {
+    expect(existy(false)).toEqual(true);
+    done();
+  });
+
 });
 
 describe('Truthy', function(done) {
